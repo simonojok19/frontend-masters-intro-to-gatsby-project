bf8a91e265f2476f16778f4c613d86d4591fb6a1
simonojok19 <simonojok19@gmail.com>
2021-12-12 21:04:47 +0300

    started creating site queries

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'http://localhost',
+    title: 'Frontend Master Intro to Gatsby',
+    description: 'Course',
+    image: 'file:///Users/simon/Downloads/Screenshot%202021-12-08%20at%2015.27.33.png'
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
 import * as React from 'react';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
     <main>
       <h1>Hello Frontend Masters</h1>
+      <Link to="/about">About</Link>
     </main>
   )
 }
